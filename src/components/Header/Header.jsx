517f3717b8ddab220684362b07581e61ca566e66
Niki2406 <nikiranga1998@gmail.com>
2024-04-17 21:58:48 +0530

    inital commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header =() => {
+    return(
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favourite food here</h2>
+                <p>Food is an essential aspect of our daily lives. Not only does it provide us with the necessary nutrients to sustain our bodies, but it also plays a significant role in our culture, traditions, and social gatherings. In this article, we will explore the various facets of food and its importance in shaping our lives.</p>
+                <button>View Menu</button>
+            </div>
+            
+        </div>
+        )
+}
+export default Header;
